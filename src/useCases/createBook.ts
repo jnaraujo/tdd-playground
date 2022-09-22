08cd876eee35cf79fa54a77c6c5e922779c8106d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-21 22:37:52 -0300

    🔥 chore: Improved createBook execute

--- src/useCases/createBook.ts
+++ src/useCases/createBook.ts
@@ -1,4 +1,5 @@
 import { Book } from "../entities/book";
+import { BookRepository } from "../repositories/bookRepository";
 
 interface CreateBookRequest {
   title: string;
@@ -7,8 +8,19 @@ interface CreateBookRequest {
 }
 
 export class CreateBook {
+  constructor(private bookRepository: BookRepository) {}
+
   async execute({ title, author, owner }: CreateBookRequest): Promise<Book> {
     const book = new Book(title, author, owner);
+
+    const doesAlreadyBookExist = await this.bookRepository.doesBookExist(book);
+
+    if (doesAlreadyBookExist) {
+      throw new Error("Book already exists");
+    }
+
+    await this.bookRepository.create(book);
+
     return book;
   }
 }
