1f41c1318b935f732978411a70116a70601945d5
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-21 21:54:40 -0300

    ✨ feat: Add book entity

--- src/entities/book.ts
+++ src/entities/book.ts
@@ -1,3 +1,23 @@
 export class Book {
-  private title: string;
+  private _title: string;
+  private _author: string;
+  private _owner: string;
+
+  get title(): string {
+    return this._title;
+  }
+
+  get author(): string {
+    return this._author;
+  }
+
+  get owner(): string {
+    return this._owner;
+  }
+
+  constructor(title: string, author: string, owner: string) {
+    this._title = title;
+    this._author = author;
+    this._owner = owner;
+  }
 }
