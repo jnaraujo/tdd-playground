4e8aca82e43f36fb957d7d85deaa57ad0c61fa0d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-21 22:41:03 -0300

    ✅ Improved book.spec.ts

--- src/entities/book.spec.ts
+++ src/entities/book.spec.ts
@@ -5,4 +5,6 @@ test("create an book", () => {
   const book = new Book("An book title", "An author", "An owner");
   expect(book).toBeInstanceOf(Book);
   expect(book.title).toBe("An book title");
+  expect(book.author).toBe("An author");
+  expect(book.owner).toBe("An owner");
 });
