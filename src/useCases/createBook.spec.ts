a264ca9aeffd98aa8e0501301f915d15461f3a3d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-21 22:38:10 -0300

    ✅ Improved creatBook test

--- src/useCases/createBook.spec.ts
+++ src/useCases/createBook.spec.ts
@@ -1,10 +1,51 @@
 import { describe, expect, it } from "vitest";
 import { Book } from "../entities/book";
+import { InMemoryBookRepository } from "../repositories/inMemory/inMemoryBookRepository";
+import { CreateBook } from "./createBook";
 
 describe("Create Book", () => {
   it("should be able to create an book", () => {
-    const book = new Book("An book title", "An author", "An owner");
-    expect(book).toBeInstanceOf(Book);
-    expect(book.title).toBe("An book title");
+    const bookRepository = new InMemoryBookRepository();
+    const createBook = new CreateBook(bookRepository);
+
+    expect(
+      createBook.execute({
+        title: "An book title",
+        author: "Some book author",
+        owner: "John Doe",
+      })
+    ).resolves.toBeInstanceOf(Book);
+  });
+
+  it("should be able to find an book", async () => {
+    const bookRepository = new InMemoryBookRepository();
+    const createBook = new CreateBook(bookRepository);
+
+    const book = await createBook.execute({
+      title: "An book title",
+      author: "Some book author",
+      owner: "John Doe",
+    });
+
+    const foundBook = await bookRepository.findByTitle(book.title);
+
+    expect(foundBook).toBe(book);
+  });
+
+  it("should not be able to create an book with the same title", async () => {
+    const bookRepository = new InMemoryBookRepository();
+    const createBook = new CreateBook(bookRepository);
+
+    const book = {
+      title: "An book title",
+      author: "Some book author",
+      owner: "John Doe",
+    };
+
+    await createBook.execute(book);
+
+    expect(createBook.execute(book)).rejects.toThrowError(
+      "Book already exists"
+    );
   });
 });
