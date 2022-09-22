b68412ef20dbcb443fea1f344a2caefb6a397e05
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-21 21:54:57 -0300

    ✅Add Book entity test

---
+++ src/entities/book.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Book } from "./book";
+
+test("create an book", () => {
+  const book = new Book("An book title", "An author", "An owner");
+  expect(book).toBeInstanceOf(Book);
+  expect(book.title).toBe("An book title");
+});
